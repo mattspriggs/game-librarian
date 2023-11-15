3d98893c1779777421bfb89754040e8a0c103db1
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-15 20:12:44 +1300

    getGameById and addGame function for the DB added

--- server/db/games.ts
+++ server/db/games.ts
@@ -5,3 +5,13 @@ export async function getAllGames(): Promise<Games[]> {
   const games = await db('games').select()
   return games
 }
+
+export async function getGameById(gamesId: number): Promise<Games> {
+  const game = await db('games').where('id', gamesId).select('*').first()
+  return game
+}
+
+export async function addGame(newGame: GamesData): Promise<Games> {
+  const [game] = await db('games').insert(newGame).returning('*')
+  return game
+}
