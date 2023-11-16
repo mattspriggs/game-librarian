a2281202ea01b433bfe4f2fc9cfe6310afc20e4f
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-16 14:09:07 +1300

    Set up App Component and working on client api

---
+++ client/apis/games.ts
@@ -0,0 +1,2 @@
+import request from 'superagent'
+import { Games, GamesData } from '../../models/games'
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,38 @@
+import { Link, Outlet } from 'react-router-dom'
+import { getAllGames } from '../../server/db/games'
+import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+
 function App() {
+  const {
+    data: gamesList,
+    isError,
+    isLoading,
+  } = useQuery({ queryKey: ['games'], queryFn: getAllGames })
+  if (isError) {
+    return <div>There was an error while getting your games</div>
+  }
+  if (!gamesList || isLoading) {
+    return <div>Loading your games...</div>
+  }
+
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1>Video Game Collection</h1>
       </header>
-      <section className="main">{/* add your code here */}</section>
+      <section className="main">
+        <ul>
+          {gamesList.map((game) => (
+            <li key={game.id}>
+              {game.title} on {game.platform}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <div>
+        <Outlet />
+      </div>
     </>
   )
 }
