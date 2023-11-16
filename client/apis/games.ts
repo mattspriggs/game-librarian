5e03cd9df8c15f9442de5e8521d7bf65441b955f
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-17 08:46:29 +1300

    Creating input form

--- client/apis/games.ts
+++ client/apis/games.ts
@@ -2,7 +2,21 @@ import request from 'superagent'
 import { Games, GamesData } from '../../models/games'
 
 const serverUrl = '/api/v1/games'
+
+// GET /api/v1/games
 export async function getGames(): Promise<Games[]> {
   const response = await request.get(serverUrl)
   return response.body.games
+}
+
+// GET /api/v1/games/:gameId
+export async function getGameById(gameId: string): Promise<Games> {
+  const response = await request.get(`${serverUrl}/${gameId}`)
+  return response.body.game
+}
+
+// POST /api/v1/games
+export async function addGame(newGame: GamesData): Promise<Games> {
+  const response = await request.post(serverUrl).send({ newGame })
+  return response.body.game
 }
--- client/components/GamesForm.tsx
+++ client/components/GamesForm.tsx
@@ -1,4 +1,31 @@
-import { Link, Outlet } from 'react-router-dom'
-import { getGames } from '../apis/games'
-import { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addGame } from '../apis/games'
+import { GamesData, Games } from '../../models/games'
+
+const initialFormData = {
+  title: '',
+  platform: '',
+}
+
+export default function GameForm() {
+  const [form, setForm] = useState<GamesData>(initialFormData)
+
+  const queryClient = useQueryClient()
+
+  const gameAddMutation = useMutation({
+    mutationFn: addGame,
+    onSuccess: (newGame) => {
+      const currentGame = queryClient.getQueryData<Games[]>(['game'])
+      if (currentGame) {
+        queryClient.setQueryData(['game'], [...currentGame, newGame])
+      }
+    },
+  })
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target
+    const newForm = { ...form, [name]: value }
+    setForm(newForm)
+  }
+}
