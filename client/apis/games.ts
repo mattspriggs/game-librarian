a94b4394dd5a2ed1a119cad0cda9a240548703c0
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-16 19:06:46 +1300

    Rendering list of games on the App component

--- client/apis/games.ts
+++ client/apis/games.ts
@@ -1,2 +1,8 @@
 import request from 'superagent'
 import { Games, GamesData } from '../../models/games'
+
+const serverUrl = '/api/v1/games'
+export async function getGames(): Promise<Games[]> {
+  const response = await request.get(serverUrl)
+  return response.body.games
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from 'react-router-dom'
-import { getAllGames } from '../../server/db/games'
+import { getGames } from '../apis/games'
 import { useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 
@@ -8,14 +8,14 @@ function App() {
     data: gamesList,
     isError,
     isLoading,
-  } = useQuery({ queryKey: ['games'], queryFn: getAllGames })
+  } = useQuery({ queryKey: ['games'], queryFn: getGames })
   if (isError) {
     return <div>There was an error while getting your games</div>
   }
   if (!gamesList || isLoading) {
     return <div>Loading your games...</div>
   }
-
+  console.log(gamesList)
   return (
     <>
       <header className="header">
