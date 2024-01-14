1e04544814ba34b1fe25470054a8738e2221c0a3
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-15 12:53:31 +1300

    Returning games in alpha order by title and then platform

--- server/db/games.ts
+++ server/db/games.ts
@@ -2,7 +2,7 @@ import db from './connection'
 import { Games, GamesData } from '../../models/games'
 
 export async function getAllGames(): Promise<Games[]> {
-  const games = await db('games').orderBy('title')
+  const games = await db('games').orderBy(['title', { column: 'platform' }])
   return games
 }
 
