09a13150cca92531a7742ff0247d7381cc958a0d
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-23 16:39:47 +1300

    Query not invalidating and reloading gamesList from DB on edit, add and delete

--- client/components/GamesList.tsx
+++ client/components/GamesList.tsx
@@ -1,11 +1,9 @@
 import { Link } from 'react-router-dom'
 import { getGames } from '../apis/games'
 import { useQuery } from '@tanstack/react-query'
-import { ChangeEvent, useState, FormEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { Games } from '../../models/games'
 // Need to add pagination to display 20 games at a time
-// Need to manage state and re-render when the select is used to show change in platform
-// May need to switch to working with props and a new component or perhaps using a different route
 // Maybe paginate alphabetically?
 
 export default function GamesList() {
@@ -15,7 +13,6 @@ export default function GamesList() {
     isLoading,
   } = useQuery({ queryKey: ['games'], queryFn: getGames })
 
-  const [form, setForm] = useState<Games[]>(gamesList as Games[])
   const [platform, setPlatform] = useState(gamesList as Games[])
   if (isError) {
     return <div>There was an error while getting your games</div>
@@ -24,48 +21,30 @@ export default function GamesList() {
   if (!gamesList || isLoading) {
     return <div>Loading your games...</div>
   }
-  console.log(gamesList)
-  console.log('From set form', form)
-  console.log('From set platform', platform)
 
   function platformList(platform: string) {
     const result = gamesList?.filter((game) => game.platform === platform)
     return result
   }
-  // console.log('platform list', platformList('Nintendo Switch'))
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const platformSelected = event.target.value
-    console.log(platformSelected)
     if (platformSelected === '') {
       setPlatform(gamesList as Games[])
     } else {
       const newList = platformList(platformSelected)
-      console.log('filterd list from select', newList)
-      setForm(newList as Games[])
       setPlatform(newList as Games[])
     }
   }
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    // const platformSelected = event.target
-    // console.log('platform selected', platformSelected)
-    // const newList = platformList(platformSelected)
-    // console.log('filterd list from select', newList)
-    // setForm(newList as Games[])
-  }
-
   return (
     <section className="main">
-      <form onSubmit={handleSubmit}>
+      <form>
         <label htmlFor="platform">Games by Platform </label>
-
         <select
           name="platform"
           id="platform"
           onChange={handleChange}
-          // valu}={platform}
           aria-label="Game list by platform selection"
           required
         >
@@ -83,7 +62,7 @@ export default function GamesList() {
         </select>
 
         <ul>
-          {!platform && gamesList ? (
+          {!platform ? (
             <>
               {gamesList.map((game) => (
                 <li key={game.id}>
