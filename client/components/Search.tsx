5f484d6f59a84498b419c4e2da9e331c7dbfa77e
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-31 12:19:39 +1300

    Added imports to the search component

--- client/components/Search.tsx
+++ client/components/Search.tsx
@@ -1,3 +1,24 @@
+import { Link } from 'react-router-dom'
+import { getGames } from '../apis/games'
+import { useQuery } from '@tanstack/react-query'
+import { ChangeEvent, useState } from 'react'
+import { Games } from '../../models/games'
+
 export default function Search() {
+  const {
+    data: gamesList,
+    isError,
+    isLoading,
+  } = useQuery({ queryKey: ['games'], queryFn: getGames })
+
+  const [games, setGames] = useState<Games[]>(gamesList as Games[])
+  if (isError) {
+    return <div>There was an error while getting your games</div>
+  }
+
+  if (!gamesList || isLoading) {
+    return <div>Loading your games...</div>
+  }
+
   return <h2>Search</h2>
 }
